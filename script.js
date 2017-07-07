77f972b390056bf4a8179a090819d739a93485e4
Hassan1161998 <Hassan_11_6_1998@hotmail.com>
2017-07-07 14:23:42 +0200

    first version

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$('#random').on('click',function(){
+window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
+})
+$('#sumbit').on('click',function(){
+var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+$('#sent').val()+"&format=json&callback=wikiCallbackFunction";
+
+            $.ajax(wikiUrl, {
+                dataType: "jsonp",
+                success: function( wikiResponse ) {
+                    for(var i =0;i<wikiResponse[1].length;i++){
+                    $('.main').append("<a target='_blank' href="+wikiResponse[3][i]+"><div class='item' >"+wikiResponse[1][i]+"<br>"+wikiResponse[2][i]+"</div></a>");
+                    }
+                }
+            });
+});
+
+            });
